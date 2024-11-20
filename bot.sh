e68e728ccb612037e36c54df7f8f18b336ec2128
erfty125 <erfty2402@gmail.com>
2024-11-21 01:39:21 +0300

    coolscript

--- bot.sh
+++ bot.sh
@@ -1,41 +1,29 @@
-#!/bin/bash
-TOKEN="fard"
+#!/bin/sh
+TOKEN="augh"
 URLC="https://api.telegram.org/bot$TOKEN/getUpdates"
 URLP="https://api.telegram.org/bot$TOKEN/sendMessage"
-#what no! stop!
+MESSAGE="hello"
 slovo='"text":"sayhello"}}'
-#message date, just for checking if we seen this message before
-MSGDATE=0
+PM="ahh"
+
+
 while true; do
-resp=$(curl -s $URLC \
-| tail -n1)
-#getting last message(so, bot doesnt work properly with message flooding)
-#echo $resp
-if [[ "$resp" == *"$slovo"* ]]; then
-res=$resp
-chatid=$(
-echo $res | awk -F \
-'"chat":{"id":|,"title"'\
- '{print $2}'\
-)
-date=$(
-echo $res | awk -F \
-'"date":|,"text"'\
- '{print $2}'\
-)
+rm getUpdates
+wget "$URLC"
+resp=$(cat getUpdates | tail -n1)
+if [ "$resp" =  "$PM" ];then
+echo "fard"
+else
+if echo $resp | grep -q "$slovo"; then
+sleep 2s
+chatid=${resp##*'"chat":{"id":'}
+chatid=${chatid%%',"first_name"'*}
 
-echo $date
-echo $chatid
+rm sendMessage
+wget --post-data="chat_id=$chatid&text=$MESSAGE" "$URLP"
 
-if [[ "$MSGDATE" != "0" ]]; then
-if [[ "$MSGDATE" != *"$date"* ]]; then
-MSG=$(shuf -n1 slova)
-curl -s -X POST $URLP -d chat_id=$chatid -d text="$MSG"
-#send hello
 fi
 fi
-export MSGDATE="$date"
-fi
-#uhhh working not well with 0.1s and less
+PM="$resp"
 sleep 1s
 done
